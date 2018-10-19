9436cdb2f3e39e1616239ab2a01d373f47f2ddf3
oliver bonhamcarter <obonhamcarter@allegheny.edu>
2018-10-19 13:54:37 -0400

    starter

---
+++ src/analysis.r
@@ -0,0 +1,54 @@
+# Your Name:
+# Names of members of your group:
+# Date:
+
+
+
+# Run the below only if the library is not already installed.
+
+library(dplyr)
+library(tidyverse)
+
+# Begin by loading your csv file from the supplimental/ directory.
+
+poliSciData <- read.table(file.choose(), header = TRUE, sep = ",")
+
+# Your group's questions from after a study of the data.
+
+#Question 1.
+
+# Analysis to respond to the question.
+
+######################################
+#Question 2.
+######################################
+
+# Analysis to respond to the question.
+
+
+
+######################################
+#Question 3.
+######################################
+
+# Analysis to respond to the question.
+
+
+
+######################################
+#Question 4.
+######################################
+
+# Analysis to respond to the question.
+
+
+
+######################################
+#Question 5.
+######################################
+
+
+# Analysis to respond to the question.
+
+
+######################################
---
+++ writing/reportAndReflections.md
@@ -0,0 +1,14 @@
+# Name
+# Date:
+
+Question 7.
+
+
+
+Question 8.
+
+
+Question 9.
+
+
+Question 10.
